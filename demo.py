f126d447ee63ac66c5a81203eeda4c80327e4a27
Matthew Cutrona <matthewbcutrona@gmail.com>
2024-12-10 13:45:47 -0500

    Adding a script that can be used in live demo for presentation

--- demo.py
+++ demo.py
@@ -1,20 +1,27 @@
-# use haarcascades to detect faces
 # Import the OpenCV library
 import cv2
 from datetime import datetime
-import time
 
 # Load the pre-trained Haar Cascade model for face detection
-face_cascade = cv2.CascadeClassifier('cars.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # Open a connection to the first camera (usually the built-in webcam)
 cap = cv2.VideoCapture(0)
 
-# Start an infinite loop to continuously capture frames from the camera every minute
+def save_image():
+    # Save the image to the 'Demo' folder with the timestamp as the name
+    # Get the current time and format it as a string
+    timestamp = datetime.now().strftime('%m-%d-%y_%H-%M-%S')
+    cv2.imwrite(f'Demo/{timestamp}.jpg', frame)
 
-# Set the time interval for capturing frames (in seconds)
-time_interval = 5
+    # Add the data to the log file
+    # add the image time and the number of faces detected to log.txt file
+    with open('Demo/log.txt', 'a') as f:
+        f.write(f'{timestamp}\t{quads[0]}\t{quads[1]}\t{quads[2]}\t{quads[3]}\n')
 
+
+
+# Start an infinite loop to continuously capture frames from the camera
 while True:
     # Capture a single frame from the camera
     ret, frame = cap.read()
@@ -38,8 +45,8 @@ while True:
     # Draw a rectangle around the detected faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        # Detect what quadrent the faces are in
 
+        # Detect what quadrent the faces are in
         # Get the center of the face
         face_x = x + w // 2
         face_y = y + h // 2
@@ -53,24 +60,17 @@ while True:
             quads[2] = True
         else:
             quads[3] = True
+    
+    # Display the captured frame in a window named 'frame'
+    cv2.imshow('frame', frame)
 
-    # Save the image to the 'images' folder with the timestamp as the name
-    # Get the current time and format it as a string
-    timestamp = datetime.now().strftime('%m-%d-%y_%H-%M-%S')
-    cv2.imwrite(f'images/{timestamp}.jpg', frame)
+    # Save the image if the 's' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        save_image()
 
-    # add the image time and the number of faces detected to log.txt file
-    with open('images/log.txt', 'a') as f:
-        f.write(f'{timestamp}\t{quads[0]}\t{quads[1]}\t{quads[2]}\t{quads[3]}\n')
-
-    # Display the captured frame in a window named 'frame' Good for debugging
-    # cv2.imshow('frame', frame)
-
-    # Wait for a key press and check if the key is 'q'
+    # Check if the 'q' key is pressed; if so, break the loop and exit
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    
-    time.sleep(time_interval)
 
 # Release the camera resource
 cap.release()
